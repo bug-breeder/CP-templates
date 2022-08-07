ca77f0dd26c9d804e01d48dfbb3112cb3eb5d5ec
anh-ngn <86034133+anh-ngn@users.noreply.github.com>
2022-08-07 20:33:21 +0700

    edit dsu

--- graph/union_find.cpp
+++ graph/union_find.cpp
@@ -1,13 +1,29 @@
-int Find(int x){
-	if (x==par[x]) return x;
-	return par[x] = Find(par[x]);
-}
- 
-bool Union(int a, int b){
-	int x = Find(a), y = Find(b);
-	if (x==y) return false;
-	if (x > y) swap(x, y);
-	par[y] = x;
-	return true;
-}
- 
+struct dsu
+{
+	vector<int> par;
+
+	dsu(int n)
+	{
+		par.resize(n + 1);
+		for (int i = 0; i <= n; i++)
+			par[i] = i;
+	}
+
+	int find(int x)
+	{
+		if (x == par[x])
+			return x;
+		return par[x] = find(par[x]);
+	}
+
+	bool merge(int a, int b)
+	{
+		int x = find(a), y = find(b);
+		if (x == y)
+			return false;
+		if (x > y)
+			swap(x, y);
+		par[y] = x;
+		return true;
+	}
+};
