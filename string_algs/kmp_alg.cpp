7e7e85e107337daf0899b548d7d25606c73130c1
anh-ngn <86034133+anh-ngn@users.noreply.github.com>
2022-12-02 20:27:26 +0700

    Update kmp_alg.cpp

--- string_algs/kmp_alg.cpp
+++ string_algs/kmp_alg.cpp
@@ -1,31 +1,15 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-using namespace std;
- 
-string s, t;
-int p[2000005];
- 
-int main(int argc, char const *argv[])
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-	
-	cin >> s;
-	cin >> t;
-	s= '#' + t + "#" + s;
-	//kmp, danh so tu 1
-	int k = 0;
-	for(int i=2; i<(int)s.size(); i++){
-		while (k && s[i]!= s[k+1]) k = p[k];
-		if (s[i] == s[k+1]) k++;
-		p[i] = k;
-	}
-	//dem so lan xuat hien cua xau t trong xau s
-	int res = 0;
-	for(int i=1; i<(int) s.size(); i++){
-		if (p[i] == (int)t.size()) res++;
-	}
-	cout << res;
-	return 0;
+vector<int> prefix_function(string s) {
+// lps[i] = the longest proper prefix of s[0..i] which is also a suffix of s[0..i].
+// Start at index 0
+    int n = (int)s.length();
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i-1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j-1];
+        if (s[i] == s[j])
+            j++;
+        pi[i] = j;
+    }
+    return pi;
 }
