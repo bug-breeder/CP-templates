c79ca3cb467521d55b9b1cfd75b7e6289d706583
anh-ngn <86034133+anh-ngn@users.noreply.github.com>
2022-12-02 20:32:57 +0700

    Update z_alg.cpp

--- string_algs/z_alg.cpp
+++ string_algs/z_alg.cpp
@@ -1,27 +1,13 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-using namespace std;
- 
-string s;
-std::vector<int> z;
- 
-int main(int argc, char const *argv[])
-{
-	cin >> s;
-	int n = s.size();
-	z.resize(n,0);//mang Z
- 
-	z[0] = n;
-	int l =0, r= 0 ;
-	for(int i=1; i<n; i++){
-		int x = max(0, min(r-i+1, z[i-l]));
-		while (i+x < n && s[x] == s[i+x]) x++;
-		z[i] = x;
-		if (x + i -1> r){
-			l = i;
-			r = i + x -1;
-		}
-	}
-    // danh so tu 0
+vector<int> z_function(string s) {
+    int n = (int) s.length();
+    vector<int> z(n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r)
+            z[i] = min (r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
+    }
+    return z;
 }
