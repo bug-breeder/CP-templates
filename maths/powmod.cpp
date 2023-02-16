8d77a5ca48cf343675d36fe4f2b7a1c08fdec08e
anh-ngn <86034133+anh-ngn@users.noreply.github.com>
2023-02-17 01:07:31 +0700

    Update powmod.cpp

--- maths/powmod.cpp
+++ maths/powmod.cpp
@@ -1,7 +1,7 @@
-ll powMod(ll x, ll k, ll M = MOD) {
+long long powMod(long long x, long long k, long long M = MOD) {
     if (k==1) return x;
     if (k==0) return 1;
-    ll ret = powMod(x, k/2);
+    ll ret = powMod(x, k/2, M);
     ret = (ret*ret) % M;
     if (k&1) ret = ret * x % M;
     return ret;
