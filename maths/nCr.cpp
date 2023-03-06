4de443e2bd9042b6707ab7e3d5d5dee7a096c75c
anh-ngn <86034133+anh-ngn@users.noreply.github.com>
2023-03-06 13:40:47 +0700

    Update and rename ckn.cpp to nCr.cpp

--- maths/nCr.cpp
+++ maths/nCr.cpp
@@ -1,17 +1,17 @@
-ll power(ll x, ll k) {
+long long powMod(long long x, long long k) {
     if (k==1) return x;
     if (k==0) return 1;
-    ll ret = power(x, k/2);
+    long long ret = powMod(x, k/2);
     ret = (ret*ret) % MOD;
     if (k&1) ret = ret * x % MOD;
     return ret;
 }
  
-ll C(ll k, ll n) {
+long long C(long long k, long long n) {
     if (k==0) return 1;
     if (k > n) return 0;
     if (k < 0) return 0;
-    return fac[n] * power(fac[k], MOD-2) % MOD * power(fac[n-k], MOD-2) % MOD; 
+    return fac[n] * powMod(fac[k], MOD-2) % MOD * powMod(fac[n-k], MOD-2) % MOD; //use invFac to get faster
 }
 //fac[0] = 1;
 //FOR(i,1,MAX-1) fac[i] = (fac[i-1]*i) % MOD;
