24a63bbd1c315ad094d7e6d8b24e00e994fd07c7
Anh Nguyen <graphjte@gmail.com>
2023-03-11 19:35:41 +0700

    update fenwick tree

--- data_structures/fenwick_tree.h
+++ data_structures/fenwick_tree.h
@@ -2,19 +2,19 @@ struct FenwickTree {
   vector<long long> s;
   int tree_size;
 
-  inline void update(long long &target, long long &value) { target += value; }
+  inline void op(long long &target, long long &value) { target += value; }
 
   FenwickTree(int tree_size = 0, int val = 0) : tree_size(tree_size) {
     s.resize(tree_size, val);
   }
   void update(int pos, long long val) { // s[pos]+= val
     for (; pos < tree_size; pos += pos & -pos)
-      update(s[pos], val);
+      op(s[pos], val);
   }
   long long query(int pos) { // [1, pos]
     long long ret = 0;
     for (; pos > 0; pos -= pos & -pos)
-      update(ret, s[pos]);
+      op(ret, s[pos]);
     return ret;
   }
 };
