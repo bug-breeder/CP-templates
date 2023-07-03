d14d968d10bca08725677c48e28dcc1a05558983
anh-ngn <86034133+anh-ngn@users.noreply.github.com>
2023-07-03 17:42:50 +0700

    Update matching.cpp

--- graph/matching.cpp
+++ graph/matching.cpp
@@ -1,11 +1,24 @@
-bool DFS(int u){
-	if (vs[u] == cnt) return false;
-	vs[u] = cnt;
-	for(int v: g[u]){
-		if (!matchX[v] || DFS(matchX[v])){
-			matchX[v] = u;
-			return true;
-		}
-	}
-	return false;
-}
+  auto maximumMatching = [&]() -> int {
+    vector<int> match(n, -1), visTime(n, -1); //0 -> n-1
+    int cnt = 0;
+
+    function<bool(int)> DFS = [&](int u) {
+      if (visTime[u] == cnt)
+        return false;
+      visTime[u] = cnt;
+      for (int v = 0; v < n; v++) {
+        if (match[v] == -1 || DFS(match[v])) {
+          match[v] = u;
+          return true;
+        }
+      }
+      return false;
+    };
+
+    int res = 0;
+    for (cnt = 0; cnt < n; cnt++) {
+      res += DFS(cnt);
+    }
+    return res;
+  };
+
