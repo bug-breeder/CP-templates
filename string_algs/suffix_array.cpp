1130b2ff6b1090428dbbf27d5794ae2a0ff45ff1
anh-ngn <86034133+anh-ngn@users.noreply.github.com>
2023-10-06 22:40:39 +0700

    Update suffix_array.cpp

--- string_algs/suffix_array.cpp
+++ string_algs/suffix_array.cpp
@@ -1,51 +1,41 @@
-//The ith suffix is the suffix starting at position i.[0,S.size()-1]
-vector<int> suffix_array(string S) {
-	int N = S.size();
-	vector<int> sa(N), classes(N);
-	for (int i = 0; i < N; i++) {
-		sa[i] = N - 1 - i;
-		classes[i] = S[i];
-	}
-	stable_sort(sa.begin(), sa.end(), [&S](int i, int j) {
-		return S[i] < S[j];
-	});
-	for (int len = 1; len < N; len *= 2) {
-		vector<int> c(classes);
-		for (int i = 0; i < N; i++) {
-			bool same = i && sa[i - 1] + len < N
-			              && c[sa[i]] == c[sa[i - 1]]
-			              && c[sa[i] + len / 2] == c[sa[i - 1] + len / 2];
-			classes[sa[i]] = same ? classes[sa[i - 1]] : i;
-		}
-		vector<int> cnt(N), s(sa);
-		for (int i = 0; i < N; i++)
-			cnt[i] = i;
-		for (int i = 0; i < N; i++) {
-			int s1 = s[i] - len;
-			if (s1 >= 0)
-				sa[cnt[classes[s1]]++] = s1;
-		}
-	}
-	return sa;
-}
+/*
+ * sa[i] is the starting index of the suffix which
+ * is $i$'th in the sorted suffix array. 
+ * The returned vector is of size $n+1$, and \texttt{sa[0] = n}.
+ * The \texttt{lcp} array contains longest common prefixes for
+ * neighbouring strings in the suffix array:
+ * \texttt{lcp[i] = lcp(sa[i], sa[i-1])}, \texttt{lcp[0] = 0}.
+ * The input string must not contain any zero bytes.
+ * Time: O(n \log n)
+ */
 
-//lsp[i] is the lsp of sa[i], sa[i+1]
-vector<int> lcp_array(const vector<int>& sa, string S) {
-	int N = S.size();
-	vector<int> rank(N), lcp(N - 1);
-	for (int i = 0; i < N; i++)
-		rank[sa[i]] = i;
-
-	int pre = 0;
-	for (int i = 0; i < N; i++) {
-		if (rank[i] < N - 1) {
-			int j = sa[rank[i] + 1];
-			while (max(i, j) + pre < S.size() && S[i + pre] == S[j + pre])
-				++pre;
-			lcp[rank[i]] = pre;
-			if (pre > 0)
-				--pre;
-		}
-	}
-	return lcp;
-}
+struct SuffixArray {
+  vector<int> sa, lcp;
+  SuffixArray(string &s, int lim = 256) { // or basic_string<int>
+    int n = s.size() + 1, k = 0, a, b;
+    vector<int> x(s.begin(), s.end() + 1), y(n), ws(max(n, lim)), rank(n);
+    sa = lcp = y, iota(sa.begin(), sa.end(), 0);
+    for (int j = 0, p = 0; p < n; j = max(1, j * 2), lim = p) {
+      p = j, iota(y.begin(), y.end(), n - j);
+      for (int i = 0; i < n; i++)
+        if (sa[i] >= j)
+          y[p++] = sa[i] - j;
+      fill(ws.begin(), ws.end(), 0);
+      for (int i = 0; i < n; i++)
+        ws[x[i]]++;
+      for (int i = 1; i < lim; i++)
+        ws[i] += ws[i - 1];
+      for (int i = n; i--;)
+        sa[--ws[x[y[i]]]] = y[i];
+      swap(x, y), p = 1, x[sa[0]] = 0;
+      for (int i = 1; i < n; i++)
+        a = sa[i - 1], b = sa[i],
+        x[b] = (y[a] == y[b] && y[a + j] == y[b + j]) ? p - 1 : p++;
+    }
+    for (int i = 1; i < n; i++)
+      rank[sa[i]] = i;
+    for (int i = 0, j; i < n - 1; lcp[rank[i++]] = k)
+      for (k &&k--, j = sa[rank[i] - 1]; s[i + k] == s[j + k]; k++)
+        ;
+  }
+};
