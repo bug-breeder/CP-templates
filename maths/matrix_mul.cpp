0b4338aaa69dfd5a9dbb0527ffce39d595a27c29
anh-ngn <86034133+anh-ngn@users.noreply.github.com>
2023-10-07 21:03:34 +0700

    Update matrix_mul.cpp

--- maths/matrix_mul.cpp
+++ maths/matrix_mul.cpp
@@ -1,5 +1,6 @@
-vector<vector<int>> mul(vector<vector<int>> &a, vector<vector<int>> &b) {
-  vector<vector<int>> res(a.size(), vector<int>(b[0].size()));
+vector<vector<long long>> mul(vector<vector<long long>> &a,
+                              vector<vector<long long>> &b) {
+  vector<vector<ll>> res(a.size(), vector<long long>(b[0].size()));
   for (int i = 0; i < a.size(); i++) {
     for (int j = 0; j < b[0].size(); j++) {
       res[i][j] = 0;
@@ -12,10 +13,10 @@ vector<vector<int>> mul(vector<vector<int>> &a, vector<vector<int>> &b) {
   return res;
 }
 
-vector<vector<int>> pow(vector<vector<int>> mat, int k) {
+vector<vector<long long>> pow(vector<vector<long long>> mat, long long k) {
   if (k == 1)
     return mat;
-  vector<vector<int>> res = pow(mat, k / 2);
+  vector<vector<long long>> res = pow(mat, k / 2);
   res = mul(res, res);
   if (k & 1)
     res = mul(res, mat);
