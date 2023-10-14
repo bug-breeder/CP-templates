a902e74afc9dfef1a6bae0c752bc7890f766328d
anh-ngn <86034133+anh-ngn@users.noreply.github.com>
2023-10-14 16:03:17 +0700

    Create debug.cpp

---
+++ Others/debug.cpp
@@ -0,0 +1,36 @@
+//Template for debugging
+#ifndef ONLINE_JUDGE
+template <class T1, class T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p) {
+  return os << '{' << p.first << ", " << p.second << '}';
+}
+
+template <class T, class = decay_t<decltype(*begin(declval<T>()))>,
+  class = enable_if_t<!is_same<T, string>::value>>
+ostream &operator<<(ostream &os, const T &c) {
+  os << '[';
+  for (auto it = c.begin(); it != c.end(); ++it)
+    os << &", "[2 * (it == c.begin())] << *it;
+  return os << ']';
+}
+//support up to 5 args
+#define _NTH_ARG(_1, _2, _3, _4, _5, _6, N, ...) N
+#define _FE_0(_CALL, ...)
+#define _FE_1(_CALL, x) _CALL(x)
+#define _FE_2(_CALL, x, ...) _CALL(x) _FE_1(_CALL, __VA_ARGS__)
+#define _FE_3(_CALL, x, ...) _CALL(x) _FE_2(_CALL, __VA_ARGS__)
+#define _FE_4(_CALL, x, ...) _CALL(x) _FE_3(_CALL, __VA_ARGS__)
+#define _FE_5(_CALL, x, ...) _CALL(x) _FE_4(_CALL, __VA_ARGS__)
+#define FOR_EACH_MACRO(MACRO, ...)                                             \
+  _NTH_ARG(dummy, ##__VA_ARGS__, _FE_5, _FE_4, _FE_3, _FE_2, _FE_1, _FE_0)     \
+  (MACRO, ##__VA_ARGS__)
+//Change output format here
+#define watch(x) cerr << "\033[1;32m" #x " = \033[1;34m" << (x) << "\033[0m; ";
+#define dbg(...)                                                              \
+  cerr << "\033[2;31mLine " << __LINE__ << ": \033[0;m"; \
+  FOR_EACH_MACRO(watch, __VA_ARGS__)                     \
+  cerr << endl
+#else
+#define dbg(...)
+#define watch(...)
+#endif
